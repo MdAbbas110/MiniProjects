e409721363265ac66b1e19fa481aa5b277433b17
Abbas <meerabbas110@gmail.com>
2023-06-22 21:21:14 +0530

    completed the project with all Get Post Put Delete

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const apiUrl = 'https://jsonplaceholder.typicode.com/todos'
 
 const getTods = () => {
-    fetch(apiUrl + '?_limit=5')
+    fetch(apiUrl + '?_limit=10')
     .then(res => res.json())
     .then((data) => {
         data.forEach((todo) => addDataToDom(todo))
@@ -10,8 +10,9 @@ const getTods = () => {
 
 function addDataToDom(todo) {
     const div = document.createElement('div')
+    div.classList.add('todo-li')
     div.appendChild(document.createTextNode(todo.title))
-    div.setAttribute('data-id',todo.name)
+    div.setAttribute('data-id',todo.id)
 
     if (todo.completed) {
         div.classList.add('done')
@@ -37,9 +38,47 @@ const createTodo = (e) => {
       .then(data => addDataToDom(data))
 }
 
+const toggleCompleted = (e) =>{
+    if (e.target.classList.contains('todo-li')) {
+        e.target.classList.toggle('done');
+
+        upDateTodo(e.target.dataset.id, e.target.classList.contains('done'))
+        
+    }
+}
+
+const upDateTodo = (id, completed) =>{
+    fetch(`${apiUrl}/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ completed }),
+        headers:{
+            'Content-Type': 'application/json'
+        }
+    })
+    // .then((res)=> res.json())
+    // .then((data) => console.log(data))
+}
+
+const deleteTodos = (e)=> {
+    if (e.target.classList.contains('todo-li')) {
+        const id = e.target.dataset.id
+        fetch(`${apiUrl}/${id}`, {
+            method: 'DELETE', //for delete req we need not pass any body or the header its just a del so id pass is delete
+        })
+        .then(res => res.json())
+        .then(() => e.target.remove())
+    }
+}
+
+
 const init = () =>{
     document.addEventListener('DOMContentLoaded', getTods)
+
     document.querySelector('#todo-form').addEventListener('submit', createTodo )
+
+    document.querySelector('#todo-list').addEventListener('click', toggleCompleted)
+
+    document.querySelector('#todo-list').addEventListener('click' ,deleteTodos)
 }
 
 init();
